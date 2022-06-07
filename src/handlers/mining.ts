fb141cd9a2045ac526e433cb566aec17ff47ba6e
Aleksei Shugaev <a.shugaev@admitad.com>
2022-06-07 16:12:21 +0300

    mathematical part for mining

--- src/handlers/mining.ts
+++ src/handlers/mining.ts
@@ -1,43 +1,56 @@
 import Context from '@/models/Context'
-import languageMenu from '@/menus/language'
 import sendOptions from '@/helpers/sendOptions'
 import axios from "axios";
+import {calcMiningProfitInDollars} from "@/helpers/calcMiningProfitInDollars";
+
+const SECONDS_IN = {
+    day: 24 * 60 * 60,
+    week: 24 * 60 * 60 * 7,
+    month: 24 * 60 * 60 * 30,
+};
 
 export const handleMining = async (ctx: Context) => {
     const text = ctx.message?.text;
 
-    const params = text?.match(/^\/(mining) ([btc|ltc|eth]) ([\d]+)$/);
+    const params = text?.match(/^\/mining [a-z]+ (\d+)$/i);
+    /**
+     * params
+     * 0 - это вся строка
+     * 1 - название монеты
+     * 2 - мощность
+     */
 
+    /**
+     * Обработать ошбки
+     * params === null - сообщение о том, что формат не правильный и подскажем какой должен быть
+     *
+     * params[1] это валидная монета из списка ALLWED_COINS (создать)
+     */
+
+    /**
+     * TODO:
+     * - обрезать цифры до 2-х симоволов после точки
+     * - оформить сообщение 
+     * - ошибки
+     * - подключить свойю ф-цию
+     */
+    
     if(!params) {
         return ctx.replyWithLocalization('inputError', sendOptions(ctx))
     }
 
-    const ticker = params[0];
-    const count = Number(params[1]);
+    // (btc|ltc|eth)
+
+    const ticker = params[1];
+    const count = Number(params[2]);
 
     const {data: respData} = await axios('https://api.minerstat.com/v2/coins');
 
-    console.log('cts', ctx);
+    const coinData = respData.filter((el: any )=> el.coin === ticker.toUpperCase())[0];
 
+    const dayProfit = calcMiningProfitInDollars(coinData, count, SECONDS_IN.day);
+    const weekProfit = calcMiningProfitInDollars(coinData, count, SECONDS_IN.week);
+    const monthProfit = calcMiningProfitInDollars(coinData, count, SECONDS_IN.month);
 
-    const coinData = respData.filter((el: any )=> el.coin === ticker)[0];
-
-    const secInDay = 24 * 60 * 60;
-    const secInMonth = 30 * 24 * 60 * 60;
-
-    const mhToH = count * 1000000*secInMonth;
-    const rewardForMh = coinData.reward_block * mhToH;
-
-    const networkHashRageSha256 = coinData.difficulty *Math.pow(2, 32) ;
-
-    console.log(mhToH, rewardForMh, networkHashRageSha256)
-
-    const mhRevenueInLtc = rewardForMh/networkHashRageSha256;
-
-    return ctx.reply(mhRevenueInLtc + '$');
-
-    // return ctx.replyWithLocalization('language', {
-    //     ...sendOptions(ctx),
-    //     reply_markup: languageMenu,
-    // })
+    return ctx.reply(dayProfit + ' ' + weekProfit + ' ' + monthProfit + ' $' );
 }
---
+++ src/helpers/calcMiningProfitInDollars.ts
@@ -0,0 +1,24 @@
+interface CalcMiningProfiParams {
+
+}
+
+// TODO: подставить типы монеты
+/**
+ * @param {coinsData} - data from api
+ * @param {hashCount} - Mega Hash count from user massage
+ * @param {seconds} - Seconds if time period
+ *
+ * @returns - number of dollars ($)
+ */
+export const calcMiningProfitInDollars = (coinsData: any, megaHashCount: number, seconds: number): number => {
+    const networkHashRateSha256 = coinsData.difficulty * Math.pow(2, 32) / 144;
+
+    // const networkHashRateSha256 = 216000000000000000000;
+
+    const megaHashForATimePeriod = megaHashCount * seconds;
+    const hashForATimePeriod = megaHashForATimePeriod * 1000000;
+    const revenueInCrypto = hashForATimePeriod/networkHashRateSha256;
+    const revenueInDollars = revenueInCrypto * coinsData.price;
+    
+    return revenueInDollars;
+}
